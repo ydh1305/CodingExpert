700b0c8440d7598d14423c23548b83f5333c6fff
donghyun.yoon <donghyun.yoon@lge.com>
2021-04-27 21:07:31 +0900

    update

--- Pro/SWEA/D5/DP/5357/5357.cpp
+++ Pro/SWEA/D5/DP/5357/5357.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define MAX 100000
-unsigned long long S[MAX + 1];
+long long S[MAX + 1];
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -10,23 +10,21 @@ int main(void) {
     int T; cin >> T;
     for(int i = 1; i <= T; i++) {
         int N, H; cin >> N >> H;
-        memset(S, 0, sizeof(S));
         int idx = -1;
         int sum = 0;
-        for(int j = 1; j <= N; j++) {
+        for(register int j = 1; j <= N; j++) {
             int x; cin >> x;
             S[j] = S[j - 1] + x;
         }
         for(int j = 1; j <= N; j++) {
             int x; cin >> x;
             if(!x) {
-                if(idx == -1) {
-                    if(S[j] >= H) {
-                        sum++;
-                        idx = j;
-                    }
+                if(j == 1 || j == N) {
+                    sum++;
+                    idx = j;
+                    continue;
                 }
-                else if(S[j] - S[idx] >= H) {
+                if(S[j] - S[idx] >= H) {
                     sum++;
                     idx = j;
                 }
